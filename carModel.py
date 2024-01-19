e35caab1be5fdb9c1ed269629c9a51d38b340b55
Parham <parham@xertcoin.com>
2024-01-19 18:05:00 +0330

    initial commit

---
+++ carModel.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, car_data):
+        self.production_date = car_data.get('productionDate')
+        self.description = car_data.get('description')
+        self.url = car_data.get('url')
+        self.known_vehicle_damages = car_data.get('knownVehicleDamages')
+        self.model = car_data.get('model')
+        self.mileage = car_data['mileageFromOdometer'].get('value') if 'mileageFromOdometer' in car_data else None
+        self.vehicle_transmission = car_data.get('vehicleTransmission')
+        self.color = car_data.get('color')
+        self.brand = car_data['brand'].get('name') if 'brand' in car_data else None
+        self.name = car_data.get('name')
+        self.image = car_data.get('image')
+        self.category = car_data.get('category')
+        self.price_currency = car_data['offers'].get('priceCurrency') if 'offers' in car_data else None
+        self.city = car_data['web_info'].get('city_persian') if 'web_info' in car_data else None
+        self.title = car_data['web_info'].get('title') if 'web_info' in car_data else None
+        self.category_slug = car_data['web_info'].get('category_slug_persian') if 'web_info' in car_data else None
+        self.district = car_data['web_info'].get('district_persian') if 'web_info' in car_data else None
+
+        self.price = car_data['offers'].get('price') if 'offers' in car_data else None
+        # Remove trailing zeros
+        if self.price is not None:
+            self.price = float(self.price)
+            if self.price.is_integer():
+                self.price = int(self.price) / 10000000
+
+    def __str__(self):
+        return f"{self.name}\nرنگ: {self.color}\nکارکرد: {self.mileage} کیلومتر\nقیمت: {self.price} میلیلون تومان\n{self.city}-{self.district}\n{self.url}\n\n{self.image}"
---
+++ main.py
@@ -0,0 +1,72 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import time
+from telegram import Bot
+from carModel import Car
+import logging
+
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
+
+# You can replace this url to any url in the car category in divar website with every filters you want
+url = 'https://divar.ir/s/iran/car/peugeot/206/5?price=-500000000&production-year=1396-&chassis_status=both-healthy&business-type=personal&has-photo=true&cities=1764%2C1%2C866%2C14%2C1722%2C1721%2C1739%2C1740%2C850%2C1751%2C2%2C1738%2C1720%2C1753%2C1752%2C774%2C1754'
+bot = Bot(token='Your Bot Token')
+
+chat_ids = ["Your Chat ID"]
+images = set()
+sent_cars = set()
+
+def fetch_data():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # This will raise an HTTPError if the HTTP request returned an unsuccessful status code
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_tags = soup.find_all('script', {'type': 'application/ld+json'})
+        source_tags = soup.find_all('source', {'data-srcset': True})
+
+        for tg in source_tags:
+            if tg:
+                image_url = tg['data-srcset']
+                images.add(image_url)
+
+        target_script_tag = None
+
+        for tag in script_tags:
+            if not tag.has_attr('data-react-helmet'):
+                target_script_tag = tag
+                break
+
+        if target_script_tag:
+            return json.loads(target_script_tag.string)
+        return None
+    except requests.HTTPError as http_err:
+        logging.error(f'HTTP error occurred: {http_err}')  
+    except Exception as err:
+        logging.error(f'Other error occurred: {err}') 
+    return None 
+
+while True:
+    try:
+        cars_data = fetch_data()
+
+        if cars_data:
+            for i in range(len(cars_data)):
+                try:
+                    car = Car(cars_data[i])
+                    if car.url not in sent_cars:
+                        car.image = list(images)[i]
+                        for chat_id in chat_ids:
+                            bot.send_message(chat_id=chat_id, text=str(car))
+                        sent_cars.add(car.url)
+                except Exception as e:
+                    logging.error(f'Error processing car data: {e}')
+        else:
+            logging.info('No new car data found.')
+        time.sleep(60)
+    except KeyboardInterrupt:
+        logging.info('Script terminated by user.')
+        break
+    except Exception as e:
+        logging.error(f'Unexpected error in main loop: {e}')
+        time.sleep(60)  # Wait before the next iteration in case of error to avoid hammering the server or the bot API
